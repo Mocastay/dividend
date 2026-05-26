6ec44656812c3f95cf90801aaaa50363d7aedf47
Mocastay <handwerksweltmeister@gmail.com>
2026-05-26 21:06:14 +0200

    Fix cache

--- sw.js
+++ sw.js
@@ -1,20 +1,12 @@
-const CACHE = 'dividend-tracker-v2';
+const CACHE = 'dividend-tracker-v3';
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/', '/index.html'])));
   self.skipWaiting();
 });
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
   self.clients.claim();
 });
 self.addEventListener('fetch', e => {
   if (e.request.url.includes('api/divvy') || e.request.url.includes('netlify/functions')) return;
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
-      if (res && res.status === 200 && e.request.method === 'GET') {
-        caches.open(CACHE).then(c => c.put(e.request, res.clone()));
-      }
-      return res;
-    }).catch(() => caches.match('/index.html')))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
